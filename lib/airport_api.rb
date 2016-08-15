9df8c7a582d3f50c9b9f74e6b3aa6a29a40f68c1
hakimu jackson <hakimuj@gmail.com>
2016-08-15 10:18:53 -0700

    Updated api file

--- lib/airport_api.rb
+++ lib/airport_api.rb
@@ -1,12 +1,35 @@
+require 'json'
 require 'net/http'
+
+class AirportApi
+
+	def initialize
+		@uri = URI("http://services.faa.gov/airport/status/#{ARGV[0]}?format=application/json")
+	end
+
+	def grab_data
+		Net::HTTP.get(@uri)
+	end
+
+	def parse_data
+   JSON.parse(grab_data)
+	end
+
+	def get_aiport
+		parse_data["IATA"]
+	end
+
+end
+
+puts AirportApi.new.get_aiport
 
 # uri = URI('http://services.faa.gov/airport/status/PDX?format=application/json')
 
 # puts Net::HTTP.get(uri)
 
-class Foo
-	def self.speak
-		"Hi"
-	end
-end
+# class Foo
+# 	def self.speak
+# 		"Hi"
+# 	end
+# end
 
